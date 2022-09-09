df09213795649386e36e06693d5504a532a4d8fe
Victoria Gosch <maviyiyi@gmail.com>
2022-09-09 20:04:30 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/GamesController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1
+{
+    public class GamesController : Controller
+    {
+        private readonly WebApplication1Context _context;
+
+        public GamesController(WebApplication1Context context)
+        {
+            _context = context;
+        }
+
+        // GET: Games
+        public async Task<IActionResult> Index()
+        {
+              return _context.Games != null ? 
+                          View(await _context.Games.ToListAsync()) :
+                          Problem("Entity set 'WebApplication1Context.Games'  is null.");
+        }
+
+        // GET: Games/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var games = await _context.Games
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (games == null)
+            {
+                return NotFound();
+            }
+
+            return View(games);
+        }
+
+        // GET: Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Description,State,Amount,Percent_Rent,Reward_Cooler_Coins,Image")] Games games)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(games);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(games);
+        }
+
+        // GET: Games/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var games = await _context.Games.FindAsync(id);
+            if (games == null)
+            {
+                return NotFound();
+            }
+            return View(games);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,State,Amount,Percent_Rent,Reward_Cooler_Coins,Image")] Games games)
+        {
+            if (id != games.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(games);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GamesExists(games.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(games);
+        }
+
+        // GET: Games/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Games == null)
+            {
+                return NotFound();
+            }
+
+            var games = await _context.Games
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (games == null)
+            {
+                return NotFound();
+            }
+
+            return View(games);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Games == null)
+            {
+                return Problem("Entity set 'WebApplication1Context.Games'  is null.");
+            }
+            var games = await _context.Games.FindAsync(id);
+            if (games != null)
+            {
+                _context.Games.Remove(games);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GamesExists(int id)
+        {
+          return (_context.Games?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ WebApplication1/Models/Games.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Games
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public double Amount { get; set; }
+        public double Percent_Rent { get; set; }
+        public double Reward_Cooler_Coins { get; set; }
+        public string Image { get; set; }
+
+    }
+}
